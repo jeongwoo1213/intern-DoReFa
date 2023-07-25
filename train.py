91aac266190c4e3ebe6d43c6af78b80c773914ab
jeongwoo1213 <122687638+jeongwoo1213@users.noreply.github.com>
2023-07-25 11:04:58 +0900

    using dataset and argparse

--- train.py
+++ train.py
@@ -6,67 +6,81 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from model import Resnet20, QResnet20
+from model import ResNet20, QResNet20
+from dataset import *
 
+
+parser = argparse.ArgumentParser(description="PyTorch Implementation of DoReFa-Net")
+
+# argument for train settings
+parser.add_argument('--epochs', type=int, default=100, help='num of epochs')
+parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
+parser.add_argument('--batch_size', type=int, default=1, help='batch size')
+parser.add_argument('--dataset', type=str, default='cifar10', choices=('cifar10','cifar100'), help='dataset to use CIFAR10|CIFAR100')
+parser.add_argument('--num_workers', type=int, default=4, help='number of data loading workers size')
+
+
+# argument for quantization
+parser.add_argument('--weight_bits', type=int, default=1, help='weights quantization bits')
+parser.add_argument('--act_bits', type=int, default=2, help='activations quantization bits')
+
+args = parser.parse_args()
+
+
+if args.dataset == 'cifar10':
+    args.num_classes = 10
+elif args.dataset == 'cifar100':
+    args.num_classes = 100
 
 # train setting
 def train():
-    epochs = 100
-    lr = 1e-3
+    
 
-    batch_size = 16
-    num_classes = 10
+    train_loader = trainloader(args)
+    test_loader = testloader(args)
 
-    x = torch.rand(batch_size,3,32,32)
-
-    r_model = Resnet20()
-    q_model = QResnet20()
+    r_model = ResNet20(args)
+    q_model = QResNet20(args)
 
     r_criterion = nn.CrossEntropyLoss()
     q_criterion = nn.CrossEntropyLoss()
 
-    r_optimizer = optim.Adam(params=r_model.parameters(), lr=lr)
-    q_optimizer = optim.Adam(params=q_model.parameters(), lr=lr)
+    r_optimizer = optim.Adam(params=r_model.parameters(), lr=args.lr)
+    q_optimizer = optim.Adam(params=q_model.parameters(), lr=args.lr)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(device)
-    
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
+
     r_model.to(device)
     q_model.to(device)
 
-    label = torch.floor(torch.rand(batch_size)*num_classes).long()
 
-    for epoch in range(1,epochs+1):
-        x = x.to(device)
-        # x_prime = x.clone().to(device)
-        label = label.to(device)
-        # label_prime = label.clone().to(device)
-        
-        
-        r_pred = r_model(x)
-        q_pred = q_model(x)
+    # train
+    for epoch in range(1,args.epochs+1):
+        for images,labels in train_loader:
+            
+            images.to(device)
+            labels.to(device)            
+            
+            r_pred = r_model(images)
+            q_pred = q_model(images)
 
-        r_loss = r_criterion(r_pred,label)
-        q_loss = q_criterion(q_pred,label)
-        
-        r_optimizer.zero_grad()
-        q_optimizer.zero_grad()
+            r_loss = r_criterion(r_pred,labels)
+            q_loss = q_criterion(q_pred,labels)
+            
+            r_optimizer.zero_grad()
+            q_optimizer.zero_grad()
 
-        r_loss.backward()
-        q_loss.backward()
-        
-        r_optimizer.step()
-        q_optimizer.step()
+            r_loss.backward()
+            q_loss.backward()
+            
+            r_optimizer.step()
+            q_optimizer.step()
 
-        if (epoch) % 10 == 0:
-           print(f'epoch {epoch}   r loss {r_loss.item()}    q loss {q_loss.item()}')
+
+        print(f'epoch {epoch:<3}    fp loss {r_loss.item():<25}  q loss {q_loss.item()}')
            
 
-
-
-
-
-
-if __name__=="__main__":
-
+if __name__=='__main__':
     train()
+
