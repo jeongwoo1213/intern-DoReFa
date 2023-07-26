abf114dead167b3fbe5b97808d991cd92851eae1
junghyup-lee <junghyup.lee@yonsei.ac.kr>
2023-07-26 01:45:35 +0000

    add validation

--- train.py
+++ train.py
@@ -38,50 +38,70 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 # train setting
 def train():
     
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     train_loader = trainloader(args)
     test_loader = testloader(args)
 
-    r_model = ResNet20(args)
+    # r_model = ResNet20(args)
     q_model = QResNet20(args)
 
-    r_criterion = nn.CrossEntropyLoss()
+    # r_model.to(device)
+    q_model.to(device)
+
+    # r_criterion = nn.CrossEntropyLoss()
     q_criterion = nn.CrossEntropyLoss()
 
-    r_optimizer = optim.Adam(params=r_model.parameters(), lr=args.lr)
+    # r_optimizer = optim.Adam(params=r_model.parameters(), lr=args.lr)
     q_optimizer = optim.Adam(params=q_model.parameters(), lr=args.lr)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    r_model.to(device)
-    q_model.to(device)
 
 
     # train
     for epoch in range(1,args.epochs+1):
+        
+        # r_model.train()
+        q_model.train()
+
         for images,labels in train_loader:
             
-            images.to(device)
-            labels.to(device)            
+            images = images.to(device)
+            labels = labels.to(device)            
             
-            r_pred = r_model(images)
+            # r_pred = r_model(images)
             q_pred = q_model(images)
 
-            r_loss = r_criterion(r_pred,labels)
+            # r_loss = r_criterion(r_pred,labels)
             q_loss = q_criterion(q_pred,labels)
             
-            r_optimizer.zero_grad()
+            # r_optimizer.zero_grad()
             q_optimizer.zero_grad()
 
-            r_loss.backward()
+            # r_loss.backward()
             q_loss.backward()
             
-            r_optimizer.step()
+            # r_optimizer.step()
             q_optimizer.step()
 
 
-        print(f'epoch {epoch:<3}    fp loss {r_loss.item():<25}  q loss {q_loss.item()}')
-           
+        # print(f'epoch {epoch:<3}    fp loss {r_loss.item():<25}  q loss {q_loss.item()}')
+        print(f'epoch {epoch:<3}    q loss {q_loss.item()}')
+        
+        if epoch % 10 == 0:
+            
+            # r_model.eval()
+            q_model.eval()
+
+            correct = 0
+            total = 0
+            with torch.no_grad():
+                for images, labels in testloader:
+                    outputs = q_model(images)
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+
+            print(f'Accuracy of quantized network: {100 * correct / total} %')
 
 if __name__=='__main__':
     train()
