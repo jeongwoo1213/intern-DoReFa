0636e7782bc82c852e48ef72f0edb37df9531560
jeongwoo <jwchoi12131213@gmail.com>
2023-08-30 05:39:57 +0000

    code refactoring

--- train.py
+++ train.py
@@ -2,8 +2,6 @@ import os
 import time
 import argparse
 
-from tqdm import tqdm
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -39,12 +37,14 @@ if args.dataset == 'cifar10':
 elif args.dataset == 'cifar100':
     args.num_classes = 100
 
+
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 log_dir = f'../results/dorefa/ResNet20_{args.dataset}/W{args.weight_bits}A{args.act_bits}'
+
 if not os.path.exists(log_dir):
     os.makedirs(os.path.join(log_dir, 'checkpoint'))
 
@@ -53,21 +53,20 @@ def train():
     
     train_loader = trainloader(args)
     test_loader = testloader(args)
-
-    # r_model = ResNet20(args)
-    q_model = QResNet20(args)
-
-    # r_model.to(device)
-    q_model.to(device)
-
-    # r_criterion = nn.CrossEntropyLoss()
-    q_criterion = nn.CrossEntropyLoss()
-
-    # r_optimizer = optim.Adam(params=r_model.parameters(), lr=args.lr)
-    q_optimizer = optim.Adam(params=q_model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-
+        
+    if (args.weight_bits == 32) and (args.act_bits == 32):
+        model = ResNet20(args)
+    else:
+        model = QResNet20(args)
+        
+    model.to(device)
+    
+    criterion = nn.CrossEntropyLoss()
+    
+    optimizer = optim.Adam(params=model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    
     best_accuracy = 0
-
+    
     # train
     start = time.time()
     
@@ -75,71 +74,64 @@ def train():
     
     for epoch in range(1, args.epochs+1):
         
-        # r_model.train()
-        q_model.train()
-
+        model.train()
+        
         for images,labels in train_loader:
             
             images = images.to(device)
-            labels = labels.to(device)            
+            labels = labels.to(device)
             
-            # r_pred = r_model(images)
-            q_pred = q_model(images)
-
-            # r_loss = r_criterion(r_pred,labels)
-            q_loss = q_criterion(q_pred,labels)
+            pred = model(images)
             
-            # r_optimizer.zero_grad()
-            q_optimizer.zero_grad()
-
-            # r_loss.backward()
-            q_loss.backward()
+            loss = criterion(pred,labels)
             
-            # r_optimizer.step()
-            q_optimizer.step()
-
+            optimizer.zero_grad()
+            
+            loss.backward()
+            
+            optimizer.step()
+            
+            
         # validation
-
-        # r_model.eval()
-        q_model.eval()
-
-        r_correct, q_correct = 0, 0
-        r_total, q_total = 0, 0
+        
+        model.eval()
+        
+        correct = 0
+        total = 0
+        
         with torch.no_grad():
             for images, labels in test_loader:
                 
                 images = images.to(device)
                 labels = labels.to(device)
-
-                q_outputs = q_model(images)
-                _,q_predicted = torch.max(q_outputs.data, 1)
-                q_total += labels.size(0)
-                q_correct += (q_predicted == labels).sum().item()
-
-        accuracy = 100 * q_correct / q_total
+                
+                outputs = model(images)
+                _,predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                
+        accuracy = 100 * correct / total
         best_accuracy = max(accuracy, best_accuracy)
                 
         if accuracy == best_accuracy:
             best_accuracy = accuracy
             torch.save({
                 'epoch':epoch,
-                'model':q_model.state_dict(),
-                'q_optimizer':q_optimizer.state_dict(),
-                'criterion':q_criterion.state_dict()
+                'model':model.state_dict(),
+                'optimizer':optimizer.state_dict(),
+                'criterion':criterion.state_dict()
             }, os.path.join(log_dir,'checkpoint/best_checkpoint.pth'))
-
-
-
-        # print(f'epoch {epoch:<3}    fp loss {r_loss.item():<25}  q loss {q_loss.item()}')        
+            
+            
         if epoch % 5 == 0: 
             elapsed_time = time.ctime(time.time() - start).split(' ')[4]
-            print(f'Epoch {epoch:<3}    q loss {q_loss.item()}  time {elapsed_time}')
+            print(f'Epoch {epoch:<3}    loss {loss.item()}  time {elapsed_time}')
         
         if epoch % 20 == 0:
-            print(f'Accuracy of quantized network: {accuracy:.2f} %\n')
-
-
-
+            print(f'Accuracy of network: {accuracy:.2f} %\n')
+            
+            
+            
     print(f'best accuracy of W{args.weight_bits}A{args.act_bits}: {best_accuracy:.2f}')
 
 
--- train.sh
+++
@@ -1,8 +0,0 @@
-python train.py --epochs 200\
-                --lr 0.001\
-                --batch_size 512\
-                --dataset 'cifar10'\
-                --num_workers 4\
-                --weight_bits 1\
-                --act_bits 2\
-                
